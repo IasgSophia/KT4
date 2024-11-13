5d8a6cffb9078a366c9ea24525f5824ddf80e9ad
FedaSuper <fedorlohkin@gmail.com>
2024-11-13 12:50:04 +0300

    first commit

---
+++ KT4_AndreevaSophia/KT4_AndreevaSophia/Pages/All.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KT4_AndreevaSophia.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для All.xaml
+    /// </summary>
+    public partial class All : Page
+    {
+        public All()
+        {
+            InitializeComponent();
+            Init();
+        }
+        public void Init()
+        {
+            DataGrid.ItemsSource = Data.databaseEntities.GetContext().Main.ToList();
+        }
+
+        private void OneP_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.People());
+        }
+
+        private void AllP_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.People());
+        }
+    }
+}
---
+++ KT4_AndreevaSophia/KT4_AndreevaSophia/Pages/People.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KT4_AndreevaSophia.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для People.xaml
+    /// </summary>
+    public partial class People : Page
+    {
+        public People()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.All());
+        }
+    }
+}
